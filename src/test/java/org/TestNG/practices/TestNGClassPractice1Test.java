34667344730258973bf30f299f9e497786c16f88
yogeshwankhede <yogirajwankhede@gmail.com>
2022-07-25 13:06:19 +0530

    all is well

--- src/test/java/org/TestNG/practices/TestNGClassPractice1Test.java
+++ src/test/java/org/TestNG/practices/TestNGClassPractice1Test.java
@@ -19,6 +19,8 @@ public class TestNGClassPractice1Test {
 	@Test
 	public void Test2() {
 		System.out.println("Test2 -> class1");
+		System.out.println("Test3 -> class1");
+		System.out.println("Test4 -> class1");
 		
 	}
 	@Test
