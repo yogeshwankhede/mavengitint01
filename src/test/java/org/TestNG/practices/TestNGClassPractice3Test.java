2bdb84b86e4acda19f2a5673de3fc4bdd6b1e5a8
yogeshwankhede <yogirajwankhede@gmail.com>
2022-07-25 15:29:12 +0530

    branch

--- src/test/java/org/TestNG/practices/TestNGClassPractice3Test.java
+++ src/test/java/org/TestNG/practices/TestNGClassPractice3Test.java
@@ -5,16 +5,24 @@ import org.testng.annotations.Test;
 public class TestNGClassPractice3Test {
 	@Test
 	public void Test1() {
+		System.out.println("Test1 -> class1");
+		System.out.println("Test1 -> class2");
 		System.out.println("Test1 -> class3");
 		
 	}
 	@Test
 	public void Test2() {
+		System.out.println("Test1 -> class1");
+		System.out.println("Test1 -> class2");
+		System.out.println("Test1 -> class3");
 		System.out.println("Test2 -> class3");
 		
 	}
 	@Test
 	public void Test3() {
+		System.out.println("Test1 -> class1");
+		System.out.println("Test1 -> class2");
+		System.out.println("Test1 -> class3");
 		System.out.println("Test3 -> class3");
 		
 	}
